137c6d1e31ee3316333ec7706783920cec378f9a
Scott Wang <scotteau@gmail.com>
2020-05-15 06:01:18 +0800

    added app-drawer to the app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
-const App = () => {
-    return <div className={"App"}>
-        App works!
-    </div>
+const App = ({children}) => {
+    const [shouldCollapse, setShouldCollapse] = useState(false);
+
+    const onCollapse = () => setShouldCollapse(true);
+    const onExpand = () => setShouldCollapse(false);
+
+    return (
+        <div className="App">
+            <section className={"left"}>
+                <header className={"SideMenuHeader"}>
+                    <div>Logo</div>
+                    <span className="material-icons" onClick={onCollapse}>
+                menu_open
+            </span>
+                </header>
+            </section>
+            <section className={`right ${shouldCollapse ? "collapse" : "expand"}`}>
+                <header className={"content-header"}>
+                    <div className="title">
+                        {shouldCollapse && (
+                            <span className={"material-icons icon"} onClick={onExpand}>
+                menu
+              </span>
+                        )}
+                        <h2>Home</h2>
+                    </div>
+
+                    <div className={"more-content"}>-- Right section for more content to insert --</div>
+                </header>
+                <div className="content">
+                    {children}
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
