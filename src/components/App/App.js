fdf1e7fa49059816dbebddbe20df182367d01c26
Scott Wang <scotteau@gmail.com>
2020-05-15 20:21:35 +0800

    seperate navigation into seperate file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,18 +3,15 @@ import "./App.css";
 import Drawer from "../Drawer/Drawer";
 import Home from "../../pages/Home/Home";
 
-import {projects} from "../../model/model";
+import { projects } from "../../model/model";
+import Navigation from "../Navigation/Navigation";
 
 const App = () => {
-
-
-    return (
-        <div className="App">
-            <Drawer>
-                <Home projects={projects}/>
-            </Drawer>
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Drawer nav={<Navigation/>} content={<Home projects={projects} />} />
+    </div>
+  );
+};
 
 export default App;
