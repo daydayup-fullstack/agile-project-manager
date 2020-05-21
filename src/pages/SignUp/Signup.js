9a251ab458aca642dd61451e0fe6920dc739080e
Scott Wang <scotteau@gmail.com>
2020-05-21 14:15:55 +0800

    added signup page files

---
+++ src/pages/SignUp/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SignUp.css";
+
+const SignUp = () => {
+    return  <div className="App-SignUp">
+        Signup works
+    </div>
+}
+
+export default SignUp;
