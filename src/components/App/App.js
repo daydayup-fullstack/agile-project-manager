77d9e3c562a3b335c2ab0c3fe21ba08a7a21a82e
Scott Wang <scotteau@gmail.com>
2020-05-31 01:45:33 +0800

    fixed login logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Drawer from "../Drawer/Drawer";
 import Home from "../../pages/Home/Home";
 import Navigation from "../Navigation/Navigation";
-import {project, projects} from "../../model/model";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { project, projects } from "../../model/model";
+import {Switch, Route, BrowserRouter as Router, Redirect} from "react-router-dom";
 import Project from "../../pages/Project/Project";
 import LoginForm from "../LoginForm/LoginForm";
 import SignUp from "../../pages/SignUp/Signup";
@@ -15,24 +15,24 @@ const column = project.columns[columnId];
 const tasks = column.taskIds.map((id) => project.tasks[id]);
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App">
       <Router>
         {isLoggedIn ? (
           <Switch>
-            {/*<Route path={"/"}>*/}
-            {/*  <SignUp />*/}
-            {/*</Route>*/}
-
             <Drawer nav={<Navigation />}>
+              <Route exact path={"/"}>
+                <Redirect to={"/home"} />
+              </Route>
+
               <Route path={"/home"}>
                 <Home projects={projects} />
               </Route>
 
               <Route path={"/tasks"}>
-                  <MyTasks tasks={tasks}/>
+                <MyTasks tasks={tasks} />
               </Route>
 
               <Route path={"/inbox"}>
@@ -45,7 +45,7 @@ const App = () => {
             </Drawer>
           </Switch>
         ) : (
-          <Route path={"/#login"}>
+          <Route exact path={"/"}>
             <LoginForm handleLogin={setIsLoggedIn} />
           </Route>
         )}
@@ -53,7 +53,6 @@ const App = () => {
     </div>
   );
 };
-
 
 const Inbox = () => {
   return <div>Inbox works!</div>;
