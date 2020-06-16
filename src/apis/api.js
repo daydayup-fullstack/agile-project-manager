0d984e88b29ff94308a9bcfa20a9330af289884a
Scott Wang <scotteau@gmail.com>
2020-06-16 16:46:50 +0800

    implemented - delete task

--- src/apis/api.js
+++ src/apis/api.js
@@ -68,13 +68,7 @@ export const updateTaskToServer = async (task) => {
 
 export const saveNewProjectToServer = async (newProject, workspace) => {
     try {
-        const {
-            id,
-            colorIndex,
-            iconIndex,
-            columnOrder,
-            name,
-        } = newProject;
+        const {id, colorIndex, iconIndex, columnOrder, name} = newProject;
 
         const response = await backend.post(`/projects`, {
             id,
@@ -83,8 +77,25 @@ export const saveNewProjectToServer = async (newProject, workspace) => {
             iconIndex,
             columnOrder,
             workspace: workspace.id,
-            projectOrder: workspace.projectsInOrder
+            projectOrder: workspace.projectsInOrder,
         });
+        console.log(response);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const saveNewTaskToServer = async (newTask) => {
+    try {
+        // const {} = newTask;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const deleteTaskFromServer = async (task) => {
+    try {
+        const response = await backend.delete(`/tasks/${task.id}`);
         console.log(response);
     } catch (e) {
         console.log(e);
