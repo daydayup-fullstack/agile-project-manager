86de472da727d1387b37a22c9eaf5cad16fb9a51
Scott Wang <scotteau@gmail.com>
2020-06-19 16:31:46 +0800

    fixed crashing bug after creating new project if return to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,44 +3,48 @@ import "./Home.css";
 import Panel from "../../components/Panel/Panel";
 import ProjectCard from "../../components/ProjectCard/ProjectCard";
 import AddProjectCard from "../../components/AddProjectCard/AddProjectCard";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom"
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
-const Home = ({ starredProjects, projects }) => {
-  const favorites = projects.filter(
-    (project) => starredProjects.indexOf(project.id) >= 0
-  );
+const Home = ({starredProjects, projects}) => {
+    const favorites = projects.filter((project) => {
+        if (project) {
+            return starredProjects.indexOf(project.id) >= 0;
+        } else {
+            return null;
+        }
+    });
 
-  return (
-    <div className={"App-Home"}>
-      {starredProjects && starredProjects.length > 0 && (
-        <Panel panelName={"Favorites"}>
-          {favorites.map((project) => (
-            <ProjectCard project={project} starred={true} key={project.id} />
-          ))}
-        </Panel>
-      )}
-      <Panel panelName={"Recent Projects"}>
-        {projects.map((project) => (
-          <ProjectCard
-            project={project}
-            starred={starredProjects.indexOf(project.id) >= 0}
-            key={project.id}
-          />
-        ))}
-        <Link to={"/create-project"}>
-          <AddProjectCard />
-        </Link>
-      </Panel>
-    </div>
-  );
+    return (
+        <div className={"App-Home"}>
+            {starredProjects && starredProjects.length > 0 && (
+                <Panel panelName={"Favorites"}>
+                    {favorites.map((project) => (
+                        <ProjectCard project={project} starred={true} key={project.id}/>
+                    ))}
+                </Panel>
+            )}
+            <Panel panelName={"Recent Projects"}>
+                {projects.map((project) => (
+                    <ProjectCard
+                        project={project}
+                        starred={starredProjects.indexOf(project.id) >= 0}
+                        key={project.id}
+                    />
+                ))}
+                <Link to={"/create-project"}>
+                    <AddProjectCard/>
+                </Link>
+            </Panel>
+        </div>
+    );
 };
 
 const mapStateToProps = (state) => {
-  return {
-    starredProjects: state.user.starredProjects,
-    projects: state.allProjects,
-  };
+    return {
+        starredProjects: state.user.starredProjects,
+        projects: state.allProjects,
+    };
 };
 
 export default connect(mapStateToProps, {})(Home);
