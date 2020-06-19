ec383de4b064ed80e90f55d9d85c818eafedaff6
Scott Wang <scotteau@gmail.com>
2020-06-19 17:14:47 +0800

    linked duedate to database (#43)

--- src/components/CalendarPopup/CalendarPopup.js
+++ src/components/CalendarPopup/CalendarPopup.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "./CalendarPopup.css";
 import {
   format,
@@ -14,14 +14,16 @@ import {
   getDate,
   isEqual,
 } from "date-fns";
-import { chunk } from "lodash";
-import { hide_calendar_popup, set_task_due_day } from '../../actions'
-import { connect } from 'react-redux';
+import {chunk} from "lodash";
+import {hide_calendar_popup, set_task_due_day} from "../../actions";
+import {connect} from "react-redux";
+import {updateTaskToServer} from "../../apis/api";
 
 const CalenderPopup = ({
-  hide_calendar_popup,
-  calendarId,
-  set_task_due_day }) => {
+                         hide_calendar_popup,
+                         calendarId,
+                         set_task_due_day,
+                       }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const setPreviousMonth = () => {
@@ -46,154 +48,158 @@ const CalenderPopup = ({
     const startWeekday = getDay(startOfMonth(selectedDate));
     const endWeekday = getDay(endOfMonth(selectedDate));
     const gridDays = chunk(
-      [
-        ...Array.from({ length: startWeekday }).fill(null),
-        ...Array.from({ length: daysInMonth }, (_, i) =>
-          setDate(selectedDate, i + 1)
-        ),
-        ...Array.from({ length: 6 - endWeekday }).fill(null),
-      ],
-      7
+        [
+          ...Array.from({length: startWeekday}).fill(null),
+          ...Array.from({length: daysInMonth}, (_, i) =>
+              setDate(selectedDate, i + 1)
+          ),
+          ...Array.from({length: 6 - endWeekday}).fill(null),
+        ],
+        7
     );
     return gridDays;
   };
-  const handleClick = (e) =>{
+  const handleClick = (e) => {
     hide_calendar_popup();
     const dueDate = Date.parse(selectedDate) / 1000;
-    set_task_due_day({ dueDate, calendarId })
-  }
+    set_task_due_day({dueDate, calendarId});
+    updateTaskToServer({id: calendarId, dueDate: dueDate});
+  };
 
   return (
-    <div className="calendarPopup">
-      <div className="calendarPopup__before"></div>
-      <div className="calendarPopup__title">
-        <div className="calendarPopup__icons">
-          <div
-            className="calendarPopup__icons-container material-icons"
-            onClick={setPreviousYear}
-            role="button"
-          >
-            chevron_left
+      <div className="calendarPopup">
+        <div className="calendarPopup__before"/>
+        <div className="calendarPopup__title">
+          <div className="calendarPopup__icons">
+            <div
+                className="calendarPopup__icons-container material-icons"
+                onClick={setPreviousYear}
+                role="button"
+            >
+              chevron_left
+            </div>
+            <div
+                className="calendarPopup__icons-container material-icons"
+                onClick={setPreviousMonth}
+                role="button"
+            >
+              arrow_left
+            </div>
           </div>
-          <div
-            className="calendarPopup__icons-container material-icons"
-            onClick={setPreviousMonth}
-            role="button"
-          >
-            arrow_left
+          <div className="calendarPopup__month" role="heading">
+            <b>{format(selectedDate, "MMMM yyyy")}</b>
+          </div>
+          <div className="calendarPopup__icons">
+            <div
+                className="calendarPopup__icons-container material-icons"
+                onClick={setNextMonth}
+                role="button"
+            >
+              arrow_right
+            </div>
+            <div
+                className="calendarPopup__icons-container material-icons"
+                onClick={setNextYear}
+                role="button"
+            >
+              chevron_right
+            </div>
           </div>
         </div>
-        <div className="calendarPopup__month" role="heading">
-          <b>{format(selectedDate, "MMMM yyyy")}</b>
-        </div>
-        <div className="calendarPopup__icons">
-          <div
-            className="calendarPopup__icons-container material-icons"
-            onClick={setNextMonth}
-            role="button"
-          >
-            arrow_right
-          </div>
-          <div
-            className="calendarPopup__icons-container material-icons"
-            onClick={setNextYear}
-            role="button"
-          >
-            chevron_right
-          </div>
-        </div>
-      </div>
-      <table className="calendarPopup__grid" role="grid">
-        <thead>
+        <table className="calendarPopup__grid" role="grid">
+          <thead>
           <tr role="row">
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Sunday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Sunday"
             >
               <abbr title="Sunday">Su</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Monday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Monday"
             >
               <abbr title="Monday">Mo</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Tuesday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Tuesday"
             >
               <abbr title="Tuesday">Tu</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Wednesday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Wednesday"
             >
               <abbr title="Wednesday">We</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Thursday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Thursday"
             >
               <abbr title="Thursday">Th</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Friday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Friday"
             >
               <abbr title="Friday">Fr</abbr>
             </th>
             <th
-              className="calendarPopup__grid-header"
-              role="columnheader"
-              aria-label="Saturday"
+                className="calendarPopup__grid-header"
+                role="columnheader"
+                aria-label="Saturday"
             >
               <abbr title="Saturday">Sa</abbr>
             </th>
           </tr>
           {generateMonth().map((week, i) => (
-            <tr className="calendarPopup__week" key={`week-${i}`} role="row">
-              {week.map((day, i) =>
-                day ? (
-                  <td
-                    className={`calendarPopup__cell${
-                      isEqual(selectedDate, day) ? " active" : ""
-                      }`}
-                    key={`day-cell-${i}`}
-                    onClick={() => setSelectedDate(day)}
-                  >
-                    {getDate(day)}
-                  </td>
-                ) : (
-                    <td className="calendarPopup__empty" key={`day-cell-${i}`}>
-                      &nbsp;
-                    </td>
-                  )
-              )}
-            </tr>
+              <tr className="calendarPopup__week" key={`week-${i}`} role="row">
+                {week.map((day, i) =>
+                    day ? (
+                        <td
+                            className={`calendarPopup__cell${
+                                isEqual(selectedDate, day) ? " active" : ""
+                            }`}
+                            key={`day-cell-${i}`}
+                            onClick={() => setSelectedDate(day)}
+                        >
+                          {getDate(day)}
+                        </td>
+                    ) : (
+                        <td className="calendarPopup__empty" key={`day-cell-${i}`}>
+                          &nbsp;
+                        </td>
+                    )
+                )}
+              </tr>
           ))}
-        </thead>
-      </table>
-      <button
-        type="button"
-        className="calendarPopup__btn"
-        onClick={handleClick}
-      >
-        Done
-      </button>
-    </div>
+          </thead>
+        </table>
+        <button
+            type="button"
+            className="calendarPopup__btn"
+            onClick={handleClick}
+        >
+          Done
+        </button>
+      </div>
   );
 };
 
-
 function mapStateToProps(state) {
   return {
-    calendarId: state.app.ui_calendar_popup.calendarId
-  }
+    calendarId: state.app.ui_calendar_popup.calendarId,
+  };
 }
-export default connect(mapStateToProps, { hide_calendar_popup, set_task_due_day })(CalenderPopup);
+
+export default connect(mapStateToProps, {
+  hide_calendar_popup,
+  set_task_due_day,
+})(CalenderPopup);
