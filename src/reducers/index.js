1dab38bc95491c2225888515aa8b36c6c906c5f9
lawrence415610 <lawrence415610@gmail.com>
2020-06-20 09:00:28 +1000

    delete some comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,18 +4,3 @@ export * from "./workspace";
 export * from "./project";
 export * from "./task_display";
 export * from "./all_projects";
-
-// ============= APP reducers ==================
-
-// ============= USER reducers ==================
-
-
-// ============= WORKSPACE reducers ==================
-
-// ============= PROJECT reducers ==================
-// current selected project - fetching additional data
-
-// ============= New TaskDisplay reducers ===============
-
-
-// ==================== allProjects ======================
